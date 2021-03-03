3521ec4cfb38c562ac32d9427d429a880fa3d740
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-03 19:37:38 +0530

    NewsContext.js of news-app for fetching url of newsAPI.

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "16e6ffaa1cea436fb8a9ee615c2a608e";
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://newsapi.org/v2/everything?q=tesla&from=2021-03-02&sortBy=publishedAt&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
